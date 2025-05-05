2c29f058b4c38d02df802ec9492f998ed678f225
mariah alice <loboalicepereira@gmail.com>
2025-05-05 15:00:17 -0300

    teste

--- Projeto/Assets/Scripts/LightPercentage.cs
+++ Projeto/Assets/Scripts/LightPercentage.cs
@@ -1,34 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using static UnityEngine.Rendering.DebugUI;
 
 public class LightPercentage : MonoBehaviour
-{ 
-    public Slider lightBar;
+{
+    public Slider lightBar; // Changed UnityEngine.UIElements.Slider to UnityEngine.UI.Slider
     public Light[] lights;
     public Light light;
     public bool luzLigada;
-    public float maxLightIntensity = 100;
-    private float LightIntensity = 100;
+    public float maxLightIntensity = 100; // intensidade máxima da luz
+    private float LightIntensity = 0;
+
+    public void OnSliderValueChanged(float value) // valor do slider
+    {
+        Debug.Log("Valor do Slider: " + value);
+    }
+
     void Start()
     {
-        
+        if (lightBar != null)
+        {
+            // Adiciona o evento para escutar mudanças de valor
+            lightBar.onValueChanged.AddListener(OnSliderValueChanged); // UnityEngine.UI.Slider supports onValueChanged
+        }
+        else
+        {
+            Debug.LogWarning("LightBar não atribuído no Inspector!"); // checa se o slider foi atribuído
+        }
     }
 
     void Update()
     {
-        if (luzLigada)
+        if (luzLigada) // verifica a ação da luz
         {
-            
             LightIntensity -= Time.deltaTime;
             lightBar.value = LightIntensity;
             light.intensity = LightIntensity;
         }
-       
-
+        else
+        {
+            light.intensity = 0;
+        }
     }
 
-    public void TurnLight(bool l)
+    public void TurnLight(bool l) // ligar e desligar a luz pelo slider
     {
         Debug.Log("Ligou a luz");
         LightIntensity = maxLightIntensity;
