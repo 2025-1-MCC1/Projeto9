d9008003a08ef43ea385e14292f3f423bd87fbd1
Nathan-bit711 <nathansantos1901@gmail.com>
2025-05-06 10:48:02 -0300

    #29
    senguindo o personagem

---
+++ Projeto/Assets/Scripts/Follow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform targetObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(this.transform.position, targetObj.position, 10 * Time.deltaTime);
+    }
+}
