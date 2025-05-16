915b115b311d5d61a924960ea79cc9046fa3c3ce
mariah alice <loboalicepereira@gmail.com>
2025-05-16 14:39:40 -0300

    #47
    add cenas novas e hist dentro do jogo

---
+++ Src/Assets/Scripts/Cena_dois.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Cena_dois: MonoBehaviour
+{
+    public void proxcena()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Src/Assets/Scripts/Cena_hist.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+public class Cena_hist : MonoBehaviour
+{
+    public void proxcena()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
